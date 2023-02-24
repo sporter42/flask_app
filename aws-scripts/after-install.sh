400b397d33da0dc5a18ea73a0a46f4d521811be2
Shawn Porter <sporter@gmail.com>
2023-02-24 15:01:27 -0500

    no need for before-install

--- aws-scripts/after-install.sh
+++ aws-scripts/after-install.sh
@@ -2,4 +2,9 @@
 cd /home/ubuntu/flask_app
 python3 -m venv venv
 venv/bin/pip3 install -r requirements.txt
+# add soft link
+if [ ! -h /var/www/html/flask_app ]; then
+	sudo ln -sT /home/ubuntu/flask_app /var/www/html/flask_app
+fi
+# apache: enable site
 sudo a2ensite flask_app
